a86360e6d7af6940e56184a9f415f6eb2d7aba43
Brais <braisvp98@gmail.com>
2020-03-10 17:37:21 +0100

    Registro Usuarios (Falta Maquetación)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Forms\RegisterType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
+    
     /**
-     * @Route("/user", name="user")
+     * @Route("/user/register", name="register")
      */
-    public function index()
+    public function register(Request $request)
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
