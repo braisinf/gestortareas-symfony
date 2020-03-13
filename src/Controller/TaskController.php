220becd4009bb5ea066f115f54b5de2b4869208d
Brais <braisvp98@gmail.com>
2020-03-13 14:56:46 +0100

    Listado tareas y detalle maquetado y funcional

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -13,21 +13,35 @@ class TaskController extends AbstractController
      */
     public function index()
     {   
-        /*
+        
         //PRUEBA RELACIONES
         //Entity Manager
         $em = $this->getDoctrine()->getManager();
         //Repositorio (clase)
         $task_repo=$this->getDoctrine()->getRepository(Task::class);
-        //Guardar todos las tareas en array de objetos tasks
-        $tasks=$task_repo->findAll();
+        //Guardar todos las tareas en array de objetos tasks ordenadas por id de forma descendente
+        $tasks=$task_repo->findBy([],['id'=>'DESC']);
 
-        foreach($tasks as $task){
-            echo $task->getUser()->getName();
-        }
-        */
+        
+        
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks' => $tasks
         ]);
+    }
+
+    //Detalle tarea
+    //@param=tarea deseada 
+    //@return=Vista con objeto tarea 
+    /**
+     * @Route("/task/detail/{id}", name="detail")
+     */ 
+    public function detail(Task $task){
+        if(!$task){
+            return $this->redirectToRoute('/');
+        }else{
+            return $this->render('task/detail.html.twig',[
+                'task' => $task
+            ]);
+        }
     }
 }
